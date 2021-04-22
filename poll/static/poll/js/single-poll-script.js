6f74cbfe5e261b163848f646f2d3c5ccab087475
mirokrastev <mirokrastev@protonmail.com>
2021-04-22 20:06:20 +0300

    small js change
    This is a small update where I improved some JS Functionality that deals with percentages on Polls.

--- poll/static/poll/js/single-poll-script.js
+++ poll/static/poll/js/single-poll-script.js
@@ -1,17 +1,22 @@
-let hasRequested = false;
-// TODO: ON REQUEST CHANGE!
 async function getPercentView() {
-    if (hasRequested) return;
-    hasRequested = true;
-
     const answers = await(await fetch('?ajax=true')).json();
     [...document.querySelector('#id_answers').children]
         .forEach(e => {
-            const children = e.children[0];
-            const key = e.textContent.trim();
+            const key = e.querySelector('[name=answers]').nextSibling.textContent.slice(2);
             const answer = answers[key];
             const word = answer[0] === 1 ? 'Vote' : 'Votes';
-            children.innerHTML += ` (${answer[0]} ${word} - ${answer[1].toFixed(2)}%)`
+
+            const spanEl = document.createElement('span');
+            spanEl.className = 'info';
+            spanEl.textContent = ` (${answer[0]} ${word} - ${answer[1].toFixed(2)}%)`;
+
+            const lastEl = e.lastElementChild;
+            if (lastEl.tagName === 'SPAN' || lastEl.classList.contains('info')) {
+                lastEl.remove();
+            }
+
+            e.append(spanEl);
+
         }, 0)
 }
 
