d1a79e266b37bf85cdfe1526ec598240a082d08c
mirokrastev <mirokrastev@protonmail.com>
2021-05-09 15:35:22 +0300

    Single Poll API Viewer

--- poll/api/urls.py
+++ poll/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from poll.api.views import APIPollViewer
+from poll.api.views import APIPollViewer, APIViewPoll
 
 urlpatterns = [
-    path('', APIPollViewer.as_view(), name='api-poll_viewer')
+    path('', APIPollViewer.as_view(), name='api-poll_viewer'),
+    path('<int:poll_id>/<str:poll>', APIViewPoll.as_view(), name='api-view_poll'),
 ]
--- poll/api/views.py
+++ poll/api/views.py
@@ -1,22 +1,38 @@
 from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
-from rest_framework import permissions
+from rest_framework.mixins import ListModelMixin
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 from poll.api.serializers import PollSerializer
-from poll.models import Poll
+from poll.models import Poll, Answer
 
 
-class APIPollViewer(ListModelMixin, CreateModelMixin,
+class APIPollViewer(ListModelMixin,
                     GenericAPIView
                     ):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+class APIViewPoll(APIView):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.object = None
+        self.answers = None
+
+    def dispatch(self, request, *args, **kwargs):
+        self.object = Poll.objects.get(id=self.kwargs['poll_id'],
+                                       question=self.kwargs['poll'])
+        self.answers = Answer.objects.filter(question=self.object)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        data = {'question': self.object.question,
+                'answers': {
+                    num: str(value)
+                    for num, value in enumerate(self.answers, start=1)
+                }}
+        return Response(data=data, status=status.HTTP_200_OK)
